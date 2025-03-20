1023a5c37372dd8a2e310f88236e5795c4807b8b
ayaschechner <ayasch19@gmail.com>
2025-03-20 16:59:31 +0200

    Updated project with latest changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,7 +8,8 @@ INSERT INTO movies (title, genre, duration, rating, release_year) VALUES
 
 INSERT INTO showtimes (movie_id, theater, start_time, end_time, price) VALUES
 (1, 'CinemaX Hall 2', '2025-03-19 18:00:00', '2025-03-19 20:30:00', 15),
-(2, 'CinemaX Hall 2', '2025-03-19 21:00:00', '2025-03-19 23:30:00', 15);
+(2, 'CinemaX Hall 2', '2025-03-19 21:00:00', '2025-03-19 23:30:00', 15)
+ON CONFLICT (theater, start_time) DO NOTHING;
 
 INSERT INTO tickets (showtime_id, customer_name, seat_number) VALUES
 (1, 'John Doe', 5),
